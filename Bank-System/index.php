7ea39fe786a04cf05e890f6961ba0f7faed7574e
Raj Agrawal <krishagrawal343@gmail.com>
2022-09-21 22:53:47 +0530

    Banking System

---
+++ Bank-System/index.php
@@ -0,0 +1,60 @@
+<!doctype html>
+<html lang="en">
+<head>
+  <!-- Required meta tags -->
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+  <!-- Bootstrap CSS -->
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
+  <link rel="stylesheet" type="text/css" href="css/style.css">
+  <link rel="stylesheet" type="text/css" href="css/styles.css">
+  <link rel="stylesheet" type="text/css" href="css/navbar.css">
+  <title> Banking System</title>
+</head>
+
+<body>
+
+  <?php
+  include 'navbar.php';
+  ?>
+  <div class="container-fluid hero-content">
+    <div class="row align-items-center">
+      <div class="col-md-6 col-sm-12 text-center" data-aos="fade-up">
+        <h1 style="color:black;">Money transaction</h1>
+        <p class="lead muted">Easy way to transfer money from any where</p>
+        <p>Welcome</p>
+        <br>
+        <br>
+        <a class="get-started" href="createuser.php">Get Started</a>
+      </div>
+      <div class="col-md-6 col-sm-12 text-center" data-aos="fade-left">
+        <div class="hero-img-div">
+          <img class="hero-img" src="../BankingSystem/assets/media/hero-img.svg" alt="img-showing-money-transfer">
+        </div>
+      </div>
+    </div>
+  </div>
+  <div class="container-fluid">
+    <!-- Introduction section -->
+    <div class="row intro py-1" style="background-color : #6d597a;">
+      <div class="col-sm-12 col-md">
+        <div class="heading text-center my-5">
+          <h1>MONEY BANK</h1>
+          <p> Hello sir this bank can give you high security for your account and your money you can easily transfer your money and see the transaction amount.</p>
+        </div>
+      </div>
+      <div class="col-sm-12 col-md img text-center">
+        <img src="img/bank.png" class="img-fluid pt-2">
+      </div>
+    </div>
+
+  
+  <footer class="text-center mt-5 py-2"style="background-color : black;">
+    <p style="color:while;">&copy <strong> Made by</strong> <b>Sneh Agrawal</b></p>
+  </footer>
+  <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx" crossorigin="anonymous"></script>
+</body>
+
+</html>
